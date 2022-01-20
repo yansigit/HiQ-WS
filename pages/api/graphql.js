a1f171906d9baaa105abc8ede29637bba62c1f5a
Seongbin Yoon <holmes9595@mail.ulsan.ac.kr>
2022-01-20 17:44:05 +0900

    backend: Put working graphql example

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,30 +1,49 @@
-import { ApolloServer } from "apollo-server-micro";
-import {gql} from "@apollo/client";
-// import { typeDefs } from "./schemas";
-// import { resolvers } from "./resolvers";
+import { ApolloServer, gql } from 'apollo-server-micro'
 
 const typeDefs = gql`
     type Query {
-        sayHello: String
+        users: [User!]!
     }
-`;
+    type User {
+        name: String
+    }
+`
 
 const resolvers = {
     Query: {
-        sayHello(parent, args, context) {
-            return 'Hello World!';
+        users(parent, args, context) {
+            return [{ name: 'Nextjs' }]
         },
     },
-};
+}
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers })
 
+const startServer = apolloServer.start()
+
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://studio.apollographql.com'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    if (req.method === 'OPTIONS') {
+        res.end()
+        return false
+    }
+
+    await startServer
+    await apolloServer.createHandler({
+        path: '/api/graphql',
+    })(req, res)
+}
+
 export const config = {
     api: {
-        bodyParser: false
-    }
-};
-
-export default apolloServer.start().then(() => {
-    return apolloServer.createHandler({ path: "/api/graphql" })
-})
+        bodyParser: false,
+    },
+}
